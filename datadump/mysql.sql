9e722b28158a1f01b9c4caa54e94d08f31424754
KUMAR-REDDY-BAVANASI <kumar5483reddy@gmail.com>
2022-12-01 11:28:47 +0530

    added k8s job object with mysql & datadump

---
+++ datadump/mysql.sql
@@ -0,0 +1,4 @@
+CREATE TABLE test.messages (message VARCHAR(250));
+INSERT INTO test.messages VALUES ('Hello');
+INSERT INTO test.messages VALUES ('Hey');
+INSERT INTO test.messages VALUES ('Kumar');
